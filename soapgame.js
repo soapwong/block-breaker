ae5697ec181c9ae5d66468dd5362c74ebccf6200
soaphuang <45193306+soapwong@users.noreply.github.com>
2019-05-03 00:16:30 +0800

    Add files via upload

---
+++ soapgame.js
@@ -0,0 +1,56 @@
+// game
+var SoapGame = function(fps) {
+    var g = {
+        actions: {},
+        keydowns: {},
+    }
+    var canvas = document.querySelector('#id-canvas')
+    var context = canvas.getContext('2d')
+    g.canvas = canvas
+    g.context = context
+    // draw
+    g.drawImage = function(soapImage) {
+        g.context.drawImage(soapImage.image, soapImage.x, soapImage.y)
+    }
+    //events
+    window.addEventListener('keydown', function(event) {
+        g.keydowns[event.key] = true
+    })
+    window.addEventListener('keyup', function(event) {
+        g.keydowns[event.key] = false
+    })
+    // 注册
+    g.registerAction = function(key, callback) {
+        g.actions[key] = callback
+    }
+    // timer
+    window.fps = 30
+    var runloop = function() {
+        // log('fps', window.fps)
+        // events
+        var actions = Object.keys(g.actions)
+        for (var i = 0; i < actions.length; i++) {
+            var key = actions[i]
+            if (g.keydowns[key]) {
+                // if keydown, 调用被注册的 action
+                g.actions[key]()
+            }
+        }
+        // update
+        g.update()
+        // clear
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        // draw
+        g.draw()
+        // next run loop
+        setTimeout(function() {
+            runloop()
+        }, 1000/window.fps)
+    }
+
+    setTimeout(function() {
+        runloop()
+    }, 1000/fps)
+
+    return g
+}
